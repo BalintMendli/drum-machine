740a0fc684aaef991717dd2cd0e95eccf949b87c
zenott <mblevel@gmail.com>
2018-08-22 12:17:06 +0200

    Add layout

--- src/App.js
+++ src/App.js
@@ -6,9 +6,22 @@ class App extends Component {
     return (
       <div className="App">
         <div id="drum-machine">
-          <div id="drum-pad-div">gf
+          <div id="drum-pad-div">
+            <div className="drum-pad" id="Q">Q</div>
+            <div className="drum-pad" id="W">W</div>
+            <div className="drum-pad" id="E">E</div>
+            <div className="drum-pad" id="A">A</div>
+            <div className="drum-pad" id="S">S</div>
+            <div className="drum-pad" id="D">D</div>
+            <div className="drum-pad" id="Z">Z</div>
+            <div className="drum-pad" id="X">X</div>
+            <div className="drum-pad" id="C">C</div>
           </div>
-          <div id="control-div">df
+          <div id="control-div">
+            <div id="power"></div>
+            <div id="display"></div>
+            <div id="volume"></div>
+            <div id="bank"></div>
           </div>
         </div>
       </div>
