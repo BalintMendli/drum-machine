aedcb3c6f9e92fb63450ab141e1eed0be178d7c4
zenott <mblevel@gmail.com>
2018-08-22 11:31:27 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div id="drum-machine">
+          <div id="drum-pad-div">gf
+          </div>
+          <div id="control-div">df
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
