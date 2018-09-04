c5233446e8a9b5a50a6ff07d91e0638ac50a995c
zenott <mblevel@gmail.com>
2018-09-04 14:11:49 +0200

    Add sounds

--- src/App.js
+++ src/App.js
@@ -2,6 +2,27 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.audioQ1 = new Audio('https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3');
+    this.audioW1 = new Audio('https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3');
+    this.audioE1 = new Audio('https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3');
+    this.audioA1 = new Audio('https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3');
+    this.audioS1 = new Audio('https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3');
+    this.audioD1 = new Audio('https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3');
+    this.audioZ1 = new Audio('https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3');
+    this.audioX1 = new Audio('https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3');
+    this.audioC1 = new Audio('https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3');
+    this.audioQ2 = new Audio('https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3');
+    this.audioW2 = new Audio('https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3');
+    this.audioE2 = new Audio('https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3');
+    this.audioA2 = new Audio('https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3');
+    this.audioS2 = new Audio('https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3');
+    this.audioD2 = new Audio('https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3');
+    this.audioZ2 = new Audio('https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3');
+    this.audioX2 = new Audio('https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3');
+    this.audioC2 = new Audio('https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3');
+  }
   render() {
     return (
       <div className="App">
@@ -23,16 +44,19 @@ class App extends Component {
                 <input type="checkbox" />
                 <span className="slider"></span>
               </label>
+              <div id="power-switch-label">Power</div>
             </div>
             <div id="display"></div>
             <div id="volume">
               <input type="range" min="1" max="100" defaultValue="50" className="rslider" id="myRange"/>
+              <label htmlFor="myRange">Volume</label>
             </div>
             <div id="bank">
               <label className="switch">
                 <input type="checkbox" />
                 <span className="slider"></span>
               </label>
+              <div id="bank-switch-label">Bank</div>
             </div>
           </div>
         </div>
